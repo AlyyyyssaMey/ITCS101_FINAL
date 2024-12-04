a07f1f686434dd2bead9b2cf7b1828ffc12cabe0
AlyyyyssaMey <alyssamahufana29@gmail.com>
2024-12-04 14:04:50 +0800

    Add files via upload

---
+++ finals_compilation.py
@@ -0,0 +1,19 @@
+def p():
+	print("\n•••••• Examples ••••••\n\n\t------\n\tOUTPUT\n\t------")
+	print("Hello World")
+	print("Alyssa")
+	print("29")
+
+def pri():
+	print("\n\t-----\n\tINPUT\n\t-----")
+	name = "Hello World"
+	print(f"print('{name}')")
+	
+	namea = "Alyssa"
+	print(f"print('{namea}')")
+	
+	num = "29"
+	print(f"print('{num}')")
+
+
+        

606ea9486b0275f6fe15c87e99f785f32f17523a
AlyyyyssaMey <alyssamahufana29@gmail.com>
2024-12-07 19:51:08 +0800

    Update finals_compilation.py

--- finals_compilation.py
+++ finals_compilation.py
@@ -1,3 +1,8 @@
+__author__ = "ALYSSA MAE DURAL HUFANA"  
+__course__ = "BSIT-2A"
+__subject__ = "ITCS-102"
+__year__ = "2024"
+
 def p():
 	print("\n•••••• Examples ••••••\n\n\t------\n\tOUTPUT\n\t------")
 	print("Hello World")
@@ -15,5 +20,38 @@ def pri():
 	num = "29"
 	print(f"print('{num}')")
 
+def menu():
+	print("\n\t\t\t\t------------------------------------")
+	print("\t\t\t\tWELCOME TO THE MENU OF COMPILATION")
+	print("\t\t\t\t------------------------------------\n\nA compilation about PYTHON language. It is composed of different components in python such as variables, print statements, functions, loops, and many more.\nPython is a high-level, general-purpose programming language. This means it's designed to be easy to read and write, and it can be used for a wide variety of tasks. It's known for its clear syntax, which resembles natural language, making it easier for beginners to learn.\nIt's also very popular in the tech world, used for web development, data science, machine learning, scripting, and more. So, please fasten your seatbelt as we take a ride on this Python's journey. Hang on tight!")
+	print("\n\n\t\t•Main Menu•")
+	print()
+	print("\t0 - Introduction \n\t1 - Print Statements\n\t2 - Operators\n\t3 - Conditionals\n\t4 - Loops\n\t5 - Functions\n\t6 - Lists\n\t7 - Exit")
 
-        
+def menu1():
+    print("       \n\t•Main Menu•")
+    print()
+    print("0 - Introduction\n1 - Print Statements\n2 - Operators\n3 - Conditionals\n4 - Loops\n5 - Functions\n6 - Lists\n7 - Exit")
+    
+def sub1menu():
+	print("\n\t----------------\n\tPRINT STATEMENTS\n\t----------------")
+	print("\t1 - Definition\n\t2 - Code with examples\n\t3 - Back to main menu")
+
+def sub2menu():
+    print("\n\t---------\n\tOPERATORS\n\t---------")
+    print("\t1 - Definition\n\t2 - Arithmetic Operators\n\t3 - Assignment Operators\n\t4 - Comparison Operators\n\t5 - Logical Operators\n\t6 - Back to main menu")
+    
+def sub3menu():
+    print("\n\t------------\n\tCONDITIONALS\n\t------------")
+    print("\t1 - Definition\n\t2 - Code with example\n\t3 - Nested Condition\n\t4 - Back to main menu")
+
+def sub4menu():
+    print("\n\t-----\n\tLOOPS\n\t-----")
+    print("\t1 - Definition\n\t2 - For Loop\n\t3 - While Loop\n\t4 - Nested for Loop\n\t5 - Back to main menu")
+
+def sub5menu():
+    print("\n\t---------\n\tFUNCTIONS\n\t---------")
+    print("\t1 - Definition\n\t2 - Code with Examples\n\t3 - Back to main menu")
+
+def sub6menu():
+	print("\n\t-----\n\tLISTS\n\t-----\n\t1 - Definition\n\t2 - Code with Examples\n\t3 - Back to main menu")

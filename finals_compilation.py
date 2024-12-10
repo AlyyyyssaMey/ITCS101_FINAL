4df669d4d604b2d35216478a557ace53f6a0a478
Alyssa Mae Hufana <alyssamahufana29@gmail.com>
2024-12-10 16:40:32 +0800

    Update finals_compilation.py

--- finals_compilation.py
+++ finals_compilation.py
@@ -138,3 +138,139 @@ def logic():
     print(a and a)
     print("\n\t-----\n\tINPUT\n\t-----")
     print('a = "Good"\nb = "Day"\nprint(a and b)\nprint(a or b)\nprint(a and a)')
+
+def defie3():
+    print("\n\tCONDITIONALS")
+    print("Conditional statements (if, else, and elif) are fundamental programming constructs that allow you to control\nthe flow of your program based on conditions that you specify. They provide a\nway to make decisions in your program and execute different code based on those decisions.")
+    
+def defie3_code():
+    print("\n•••••• Example ••••••")
+    print("\n\n\t------\n\tOUTPUT\n\t------")
+    print("\nThe program will break if you enter a letter.")
+
+    isOn = True
+    sum=0
+
+    while isOn == True:
+        num = input("Enter a number: ")
+
+        if num.isnumeric():
+            sum += int(num)
+        else:
+            break
+    print(f"The total is {sum}")
+
+    print("\n\t-----\n\tINPUT\n\t-----")
+    print()
+
+    print('isOn = True\nsum = 0\n\nwhile isOn == True:\n   num = input("Enter a number: ")\n\n   if num.isnumeric():\n     sum += int(num)\n   else:\n     break\nprint(f"The total is {sum}")')
+
+def nest():
+	print("\nThe nested if statements in Python are the nesting of an if\nstatement inside another if statement with or without an else statement.")
+
+def nest_code():
+    print("\n•••••• Example ••••••")
+    print("\n\n\t------\n\tOUTPUT\n\t------\nPurchasing an item program.")
+    item_price = input("\nEnter the price of your item here: ")
+    qty = input("How many did you buy? ")
+    
+    if item_price.isnumeric() and qty.isnumeric():
+        total_price = int(item_price) * int(qty)
+        if total_price >= 100:
+            discount = total_price * 0.10
+            discounted_price = total_price - discount
+            print(f"Hello! We are happy to inform you that your purchase is discounted! The total price of your product is {total_price}. The discounted price is now {discounted_price}")
+        else:
+            print("You availed the original price")
+    else:
+        print("Please the numeric value of your purchase/s")
+    
+    print("\n\t-----\n\tINPUT\n\t-----")
+    print('\nitem_price = input("Enter the price of your item here: ")\nqty = input("How many did you buy? ")if item_price.isnumeric() and qty.isnumeric():\n   total_price = int(item_price) * int(qty)\n   if total_price >= 100:\n     discount = total_price * 0.10\n     discounted_price = total_price - discount\n     print(f"Hello! We are happy to inform you that your purchase is discounted! The total price of your product is {total_price}. The discounted price is now {discounted_price}")\n   else:\n    print("You availed the original price")\nelse:\n   print("Please the numeric value of your purchase/s")')
+
+def defi4():
+    print("\n\tLOOPS")
+    print("A loop is an instruction that repeats multiple times as long as some condition is met.")
+
+def loopi():
+    print("\n•For Loop - A for loop in Python is used to iterate over a sequence (list, tuple, set, dictionary, and string).")
+    print()
+    print("•••••• Example ••••••")
+    print("\n\n\t------\n\tOUTPUT\n\t------")
+    for b in range (1, 8):
+        print("\nROSE")
+    print("\n\t-----\n\tINPUT\n\t-----")
+    print('\nfor b in range (1, 8):\n   print("ROSE")')
+
+def louwie():
+	print("\nWhile Loop - The while loop is used to execute a set of statements as long as a condition is true.")
+
+def nestie():
+	print("\nNested For Loop - If a loop exists inside the body of another loop, it is called a nested loop.")
+    
+def nestie_code():
+    print("\n•••••• Example ••••••")
+    print("\n\n\t------\n\tOUTPUT\n\t------")
+    print()
+    for a in range (0, 5):
+        for b in range (0, 5-a):
+            print(" ", end=" ")
+        for c in range (0, 2*a+1):
+            print("*", end=" ")
+        print()
+    print("\n\t-----\n\tINPUT\n\t-----")
+    print('\nfor a in range (0, 5):\n   for b in range (0, 5-a):\n     print(" ", end=" ")\n   for c in range (0, 2*a+1):\n     print("*", end=" ")\n   print()')
+
+def defie5():
+    print("\n\tFUNCTIONS")
+    print("A function is a block of code which only runs when it is called.")
+    
+def defie5_code():
+    print("\nIn python, we create a function using the 'def' keyword.")
+    print("\n•••••• Example ••••••")
+    print("\n\n\t------\n\tOUTPUT\n\t------")
+    def hello():
+        print("Hello, Good Day! How are you today?")
+    hello()
+    print("\n\t-----\n\tINPUT\n\t-----")
+    print('def hello():\n   print("Hello, Good Day! How are you today?")\nhello()')
+
+def defie6():
+    print("\n\tLISTS")
+    print("Lists are used to store multiple items in a single variable.")
+    
+def defie6_code():
+    print("\n•••••• Example ••••••")
+    print("\n\n\t------\n\tOUTPUT\n\t------")
+    hi = ["hi" , "hello" , "hi"]
+    print(hi)
+    
+    numbers = list(range(1,9))
+    print(numbers)
+
+    n = list("Alyssa Mae Dural Hufana")
+    print(n)
+
+    print("Processing")
+
+    for x in n:
+	    print(x, end="")
+	
+    print("\n\t-----\n\tINPUT\n\t-----")
+    print('hi = ["hi", "hello", "hi"]\nprint(hi)\n\nn = list("Alyssa Mae Dural Hufana")\nprint(n)\n\nnumbers =list(range(19))\nprint(numbers)\n\nprint("Processing")\n\nfor x in n:\n   print(x, end="")')
+    
+def intro():
+    print("\n\t~~~\n\tINTRODUCTION\n\t~~~")
+    print("\nPython is a popular programming language. It was created by Guido van Rossum, and released in 1991. It is used for web development (server-side), software development, mathematics, and system scripting.")
+
+def sub_intro():
+    print("\n\t~~~\n\tOptions\n\t~~~")
+    print("\n1 - Introduction\n2 - Back to main menu")
+    
+start = input("Hello, Good day! Would you like to start the program? (Yes / No): ")
+if start.lower() == "yes":
+    print("\nGreat! The program will now start. Please wait a little bit")
+    menu()
+else:
+    print("\nThe program is now terminated Thank you for stopping by.")
+    exit()
